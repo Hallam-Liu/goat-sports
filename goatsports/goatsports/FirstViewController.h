636da5914418344d4911399582e6d5c7f0b47ab4
刘浩然 <376026488@qq.com>
2017-09-23 03:14:01 -0400

    install Pod

--- goatsports/goatsports/FirstViewController.h
+++ goatsports/goatsports/FirstViewController.h
@@ -8,8 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
+#import "AFHTTPSessionManager.h"
+#define kTimeOutInterval 30
+#pragma mark - 创建请求者
 @interface FirstViewController : UIViewController
+{
+    IBOutlet UITextField * telePhoneNumber;
+    IBOutlet UITextField * userName;
+    IBOutlet UITextField * passWord;
+    IBOutlet UITextField * verifyCode;
+    IBOutlet UIProgressView * progress;
+}
+-(IBAction)sendMessage:(id)sender;
+-(IBAction)signUp:(id)sender;
 
++(AFHTTPSessionManager *)manager;
+// 请求超时的时间
+typedef void (^SuccessBlock)(NSDictionary *dict, BOOL success); // 访问成功block
+typedef void (^AFNErrorBlock)(NSError *error); // 访问失败block
 
 @end
 
--- goatsports/goatsports/FirstViewController.m
+++ goatsports/goatsports/FirstViewController.m
@@ -13,7 +13,23 @@
 @end
 
 @implementation FirstViewController
-
+#pragma mark - 创建请求者
++(AFHTTPSessionManager *)manager
+{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    // 超时时间
+    manager.requestSerializer.timeoutInterval = kTimeOutInterval;
+    
+    // 声明上传的是json格式的参数，需要你和后台约定好，不然会出现后台无法获取到你上传的参数问题
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer]; // 上传普通格式
+    //    manager.requestSerializer = [AFJSONRequestSerializer serializer]; // 上传JSON格式
+    
+    // 声明获取到的数据格式
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer]; // AFN不会解析,数据是data，需要自己解析
+    //    manager.responseSerializer = [AFJSONResponseSerializer serializer]; // AFN会JSON解析返回的数据
+    // 个人建议还是自己解析的比较好，有时接口返回的数据不合格会报3840错误，大致是AFN无法解析返回来的数据
+    return manager;
+}  
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -25,5 +41,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+-(IBAction)sendMessage:(id)sender{
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"data" ofType:@"json"];
+    NSString *jsonStr  = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:nil];
+    NSData *jaonData   = [[NSData alloc] initWithData:[jsonStr dataUsingEncoding:NSUTF8StringEncoding]];
+    NSArray *allTrack = [NSJSONSerialization JSONObjectWithData:jaonData options:(NSJSONReadingMutableContainers) error:nil];
+    AFHTTPSessionManager *session = [AFHTTPSessionManager manager];
+    [session GET:@"http://47.94.135.66" parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        NSLog(@"成功");
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        NSLog(@"失败");
+    }];
+   
+    
+}
 
 @end
